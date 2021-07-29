dea937708e29ff311c3f070c89bb7497b3f13124
alexander <posb00@gmail.com>
2021-07-29 13:01:34 -0400

    show login or register form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -40,21 +40,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600"
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register',
+                }"
+                @click.prevent="tab = 'login'"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#"
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login',
+                }"
+                @click.prevent="tab = 'register'"
                 >Register</a
               >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -84,7 +95,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -173,6 +184,11 @@ import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'Auth',
+  data() {
+    return {
+      tab: 'login',
+    };
+  },
 
   computed: {
     ...mapState(['authModalShow']),
