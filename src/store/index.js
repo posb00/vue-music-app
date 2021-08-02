cd0e88eced6888a008c31754000abf2fbd749f0f
alexander <posb00@gmail.com>
2021-08-02 17:00:16 -0400

    action for register an user

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import { auth, userCollection } from '../includes/firebase';
 
 export default createStore({
   state: {
@@ -12,6 +13,24 @@ export default createStore({
     },
     toggleAuth(state) {
       state.userLoggedIn = !state.userLoggedIn;
+    },
+  },
+  actions: {
+    async register({ commit }, payload) {
+      await auth.createUserWithEmailAndPassword(
+        payload.email,
+        // eslint-disable-next-line comma-dangle
+        payload.password
+      );
+
+      await userCollection.add({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      commit('toggleAuth');
     },
   },
 });
