a229ad30213a1613c0104f67523b584c6e7e8156
alexander <posb00@gmail.com>
2021-08-02 14:38:03 -0400

    create a login and register component

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -63,155 +63,10 @@
               >
             </li>
           </ul>
-
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field name="email"
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </form>
+          <app-login-form v-if="tab === 'login'" />
           <!-- Registration Form -->
-          <div class="text-white text-center font-bold p-5 mb-4"
-          v-if="reg_show_alert"
-          :class="reg_alert_variant"
-          >
-          {{ reg_alert_msg }}
-          </div>
-          <vee-form
-            v-show="tab === 'register'"
-            :validation-schema="schema"
-            @submit="register"
-            :initial-values="userData"
-          >
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <vee-field
-                type="text"
-                name="name"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"
-              />
-              <ErrorMessage class="text-red-600" name="name" />
-            </div>
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field
-                type="email"
-                name="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-              <ErrorMessage class="text-red-600" name="email" />
-            </div>
-            <!-- Age -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
-              <vee-field
-                type="number"
-                name="age"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-              />
-              <ErrorMessage class="text-red-600" name="age" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <vee-field name="password" :bails="false" v-slot="{field, errors}" >
-               <input type="password"
-
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" v-bind="field"
-                 >
-                 <div v-for="error in errors" :key="error" class="text-red-600">
-                   {{error}}
-                 </div>
-              </vee-field>
-              <ErrorMessage
-                class="text-red-600"
-                name="password"
-                bails="false"
-              />
-            </div>
-            <!-- Confirm Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
-              <vee-field
-                name="confirm_password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password"
-              />
-            </div>
-            <ErrorMessage class="text-red-600" name="confirm_password" />
-            <!-- Country -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
-              <vee-field
-                as="select"
-                name="country"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-              >
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-                <option value="Antartica">Antartica</option>
-              </vee-field>
-            </div>
-            <ErrorMessage class="text-red-600" name="country" />
-            <!-- TOS -->
-            <div class="mb-3 pl-6">
-              <vee-field
-                value="1"
-                name="tos"
-                type="checkbox"
-                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
-              />
-
-              <label class="inline-block">Accept terms of service</label>
-              <br />
-              <ErrorMessage class="text-red-600" name="tos" />
-            </div>
-            <button
-              :disabled="reg_in_submission"
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </vee-form>
+          <app-register-form v-if="tab === 'register'" />
         </div>
       </div>
     </div>
@@ -220,30 +75,23 @@
 
 <script>
 import { mapMutations, mapState } from 'vuex';
+import AppRegisterForm from './RegisterForm.vue';
+import AppLoginForm from './LoginForm.vue';
 
 export default {
   name: 'Auth',
   data() {
     return {
       tab: 'login',
-      schema: {
-        name: 'required|min:3|max:100|alpha_spaces',
-        email: 'required|min:3|max:200|email',
-        age: 'required|min_value:18|max_value:120',
-        password: 'required|min:6|max:20',
-        confirm_password: 'password_mismatch:@password',
-        country: 'required|country_excluded:Antartica',
-        tos: 'tos',
-      },
-      userData: {
-        country: 'USA',
-      },
       reg_in_submission: false,
       reg_show_alert: false,
       reg_alert_variant: 'bg-blue-500',
       reg_alert_msg: 'Please wait! is beign created.',
-
     };
+  },
+  components: {
+    AppLoginForm,
+    AppRegisterForm,
   },
 
   computed: {
@@ -251,15 +99,6 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
-    register(values) {
-      this.reg_show_alert = true;
-      this.reg_in_submission = true;
-      this.reg_alert_variant = 'bg-blue-500';
-      this.reg_alert_msg = 'Please wait! Your account is being created.';
-      this.reg_alert_variant = 'bg-green-500';
-      this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(values);
-    },
   },
 };
 </script>
--- src/plugins/validation.js
+++ src/plugins/validation.js
@@ -45,11 +45,14 @@ export default {
           min_value: `The field ${ctx.field} is too low`,
           max_value: `The field ${ctx.field} is to high`,
           exclude: `You are not allowed to user this value for the field ${ctx.field}.`,
-          country_excluded: 'Due to restrictions, we do not accept users from this location.',
+          country_excluded:
+            'Due to restrictions, we do not accept users from this location.',
           password_mismatch: "The passwords don't match.",
           tos: 'Yot must accept the Terms of Service.',
         };
-        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid`;
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`;
         return message;
       },
       validateOnBlur: true,
