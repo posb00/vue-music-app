1f8bec68b7396efc17a82b5b19911d585da41a99
alexander <posb00@gmail.com>
2021-08-12 16:32:38 -0400

    css active links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,18 +3,17 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        to="/"
+        exact-active-class="no-active"
         >Music</router-link
       >
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
-          <router-link
-            class="text-white font-bold uppercase text-2xl mr-4"
-            to="/about"
-            >About</router-link
-          >
+          <router-link class="px-2 text-white" to="/about">About</router-link>
           <li v-if="!userLoggedIn">
             <a
               href="#"
